c0d6dfb47f7659712b01374ee62a52aede78d652
jibeex <jibeex@gmail.com>
2016-01-20 17:26:32 +0100

    Initial Commit

---
+++ CDFlipViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CDFlipViewDemo
+//
+//  Created by jibeex on 1/20/16.
+//  Copyright © 2016 Jianbin LIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
